43d9e15bb7ef23fda653eeb48a866db2ecd7c1ff
AFLopezG <lopez.gutierrez.a07@gmail.com>
2022-10-03 17:14:37 -0400

    inicio

---
+++ back/app/Http/Controllers/AfiliadoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Afiliado;
+use App\Http\Requests\StoreAfiliadoRequest;
+use App\Http\Requests\UpdateAfiliadoRequest;
+
+class AfiliadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreAfiliadoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreAfiliadoRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Afiliado  $afiliado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Afiliado $afiliado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Afiliado  $afiliado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Afiliado $afiliado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateAfiliadoRequest  $request
+     * @param  \App\Models\Afiliado  $afiliado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateAfiliadoRequest $request, Afiliado $afiliado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Afiliado  $afiliado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Afiliado $afiliado)
+    {
+        //
+    }
+}
---
+++ back/database/migrations/2022_10_03_185337_create_vehiculos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehiculos', function (Blueprint $table) {
+            $table->id();
+            $table->string('placa')->unique();
+            $table->string('tipo');
+            $table->string('modelo');
+            $table->string('marca');
+            $table->string('color');
+            $table->string('capacidad');
+            $table->unsignedBigInteger('afiliado_id');
+            $table->foreign('afiliado_id')->references('id')->on('afiliados');
+            $table->unsignedBigInteger('grupo_id');
+            $table->foreign('grupo_id')->references('id')->on('grupos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehiculos');
+    }
+};
