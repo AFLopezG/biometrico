af3422bfd4b8e9f1b39aff23a544003bc57b4062
adimerpaul <adimer101@gmail.com>
2023-01-05 17:52:28 -0400

    new version

--- front/src/pages/Impresion.vue
+++ front/src/pages/Impresion.vue
@@ -81,9 +81,12 @@ export default {
           <div id='print'>\
           <table>\
           <tr><td style='width:20%'><img src='imagenes/logo.png'></td>\
-          <td class='titulo1'  style='width:50%'>SINDICATO MIXTO DE TRANSPORTE<br><span class='titulo2'>26 DE JULIO</span><br><span class='titulo3' FUNDADO EL 26 DE JULIO DE 1970 <br> RESOLUCION SUPREMA 221174</span><br><br><span class='titulo2'> HOJA DE APORTES</span></td>\
+          <td class='titulo1'  style='width:50%'>SINDICATO MIXTO DE TRANSPORTE<br><span class='titulo2'>26 DE JULIO</span><br><span class='titulo3' FUNDADO EL 26 DE JULIO DE 1970 <br> RESOLUCION SUPREMA 221174</span><br><br><span class='titulo2'> ASISTENCIA</span></td>\
           <td class='titulo1'> FECHA<br>"+pago.fecha +" " +pago.hora+"<br><span class='titulo5'> N° Movil "+pago.afiliado.codigo+" </span><br><span   >No "+pago.id+"</span></td></tr>\
           </table>\
+          <table><tr>\
+          <td class='col-4 titulo4'>GRUPO<br><span class='texto1'>"+pago.grupo.tipo+"</span></td><td class='col-4 titulo4'>PLACA<br><span class='texto1'>"+pago.vehiculo.placa+"</span></td>\
+          </tr></table>\
           <div class='col-12 titulo4'>AFILIADO</div><div class='col-12 texto2'>"+pago.afiliado.nombres +' ' +pago.afiliado.apellidos+"</div>\
           <div class='titulo4'>Muchas gracias por su Asistencia</div>\
           </div><br><br>\
